65f6d5abc24deb583a475ea5233ec6b4f68badbf
slendersold <slendersold@users.noreply.github.com>
2020-06-09 03:12:14 +0300

    коммент принтера

--- src/main/java/scratches/workers/commander/Printer.java
+++ src/main/java/scratches/workers/commander/Printer.java
@@ -2,24 +2,36 @@ package scratches.workers.commander;
 
 import scratches.Vehicle;
 
+/**
+ * Класс, печающий форматированным выводом
+ *
+ * @author slendersold
+ * @version 1.1.1
+ */
 public class Printer {
-    public static void prf(Vehicle c){
-        System.out.printf("%10d",c.getId());
-        System.out.printf("%10s",c.getName());
-        System.out.printf("%1$10td.%1$tm.%1$ty %1$tH:%1$tM:%1$tS     ",c.getCreationDate());
-        System.out.printf("X:%15.3f км",c.getCoordinates().getX());
-        System.out.printf("Y:%15.3f км",c.getCoordinates().getY());
-        System.out.printf("%15.3f Дж",c.getEnginePower());
-        System.out.printf("%15d км",c.getDistanceTravelled());
+    /**
+     * Здесь работает принтер "брррррр"
+     *
+     * @param c - аргумент, который надо распечатать
+     * @see Printer#prf(Vehicle)
+     */
+    public static void prf(Vehicle c) {
+        System.out.printf("%10d", c.getId());
+        System.out.printf("%10s", c.getName());
+        System.out.printf("%1$10td.%1$tm.%1$ty %1$tH:%1$tM:%1$tS     ", c.getCreationDate());
+        System.out.printf("X:%15.3f км", c.getCoordinates().getX());
+        System.out.printf("Y:%15.3f км", c.getCoordinates().getY());
+        System.out.printf("%15.3f Дж", c.getEnginePower());
+        System.out.printf("%15d км", c.getDistanceTravelled());
         try {
             System.out.printf("%15s", c.getType().toString());
-        }catch (NullPointerException e){
-            System.out.printf("%15s","не названо");
+        } catch (NullPointerException e) {
+            System.out.printf("%15s", "не названо");
         }
         try {
             System.out.printf("%15s", c.getFuelType().toString());
-        }catch (NullPointerException e){
-            System.out.printf("%15s","не названо");
+        } catch (NullPointerException e) {
+            System.out.printf("%15s", "не названо");
         }
         System.out.println();
     }
